4ab205dedfaf40295d36ae4bf2323f0647f17a2b
SawWaddy <sawwaddytint@gmail.com>
2022-05-21 16:37:59 +0630

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:live_project/resources/colors.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: APP_THEME_COLOR,
+    );
+  }
+}
---
+++ lib/resources/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+// ignore: constant_identifier_names
+//App theme color
+const APP_THEME_COLOR = Color.fromRGBO(254, 160, 36, 1);
