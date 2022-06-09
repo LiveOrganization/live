19e9c99b1013c18c29d4b174f5a996a3ff1ed7b3
May Myat Mon <mmmon@mit.com.mm>
2022-06-09 22:52:48 +0630

    add username text field and button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,8 +6,54 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: APP_THEME_COLOR,
+    return Scaffold(
+      // backgroundColor: APP_THEME_COLOR,
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  label: Text("Username")
+                ),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              FlagButtonWidget("Log In"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FlagButtonWidget extends StatelessWidget {
+  String text;
+  FlagButtonWidget(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {},
+      child: Container(
+        width: 200.0,
+        height: 50.0,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: APP_THEME_COLOR,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Text(
+          this.text,
+          style: TextStyle(color: Colors.white, fontSize: 18.0),
+        ),
+      ),
     );
   }
 }
